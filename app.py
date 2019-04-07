7ee49b5934d0cf52e40c423c78f82cf68c0ca943
ArifulProtik <mdarifulislam.protik@gmail.com>
2019-04-07 15:10:32 +0600

    Adding Webhook

---
+++ app.py
@@ -0,0 +1,14 @@
+import os, sys 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook',methods=['GET'])
+def webhook():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "jerrymamo":
+            return "Mismatched, Get Lost", 403
+        return "What Are You Doing Here Man? You Were Not Supposed To Be here", 200
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 80)
