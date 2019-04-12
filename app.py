a65d46087fe1dd11e9df6e7315a49b501609a89b
ArifulProtik <mdarifulislam.protik@gmail.com>
2019-04-12 20:19:54 +0600

    fixing webhook

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def webhook():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == "jerrymamo":
             return "Mismatched, Get Lost", 403
-        return "What Are You Doing Here Man? You Were Not Supposed To Be here", 200
+    return "What Are You Doing Here Man? You Were Not Supposed To Be here", 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True,)
